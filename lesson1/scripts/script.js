1abaf9ac2dde01182dac07ab2929e79721bf7293
VictorKmin <kminvictor@gmail.com>
2019-08-12 22:08:02 +0300

    Initial commit

---
+++ lesson1/scripts/script.js
@@ -0,0 +1,83 @@
+let name = "Viktor";
+let age = 22;
+let car = false;
+
+//Стрічокив йкоментар
+
+/**
+ * цей метод використову.ґжсятьсяс іа аіа
+ */
+
+/* console.log(name);
+Блочний коментар
+ */
+
+
+// переоголошення та document.write vs .log
+let motorMyCar = 7;
+
+console.log(motorMyCar);
+
+motorMyCar = null;
+
+document.write(motorMyCar);
+
+
+// CONST
+const THIS_IS_CONST = 3.1415999999999999;
+
+const E = 2.71;
+
+// THIS_IS_CONST = 3;
+// console.log(THIS_IS_CONST);
+
+let one = 0.1;
+let two = 0.2;
+console.log(one + two);
+
+// DONT USE THIS PLEEEASE! :((
+var xxx = 'This is var';
+console.log('________________');
+console.log(xxx.length);
+console.log('________________');
+
+// математика!
+console.log(1 + 2);
+
+console.log(20 - 7);
+
+console.log(3 * 2);
+
+console.log(25 / 5);
+
+// console.log(10 % 3); // 1
+
+console.log(8 % 2);
+console.log(23 % 3);
+
+console.log("2" + 2);
+console.log('Okten' + 'Web');
+console.log("2" - 2);
+console.log("2" * 2);
+console.log("2" / 2);
+console.log("okten" + 2);
+console.log("okten" * 2);
+console.log(typeof NaN);
+console.log(2 + '');
+
+console.log(
+    (2 + 2) * 2
+)
+
+// string -> num; num -> string
+let stringFromNumber = (77).toString();
+console.log(stringFromNumber);
+let numberFromString = parseInt('777');
+let numberFromString2 = +'777';
+
+let okten = +'okten';
+
+console.log(numberFromString);
+console.log(numberFromString2);
+
+console.log(okten);
