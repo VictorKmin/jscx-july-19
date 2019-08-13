9ce5065645d886fbb9f914e64a50e68ad7c186e1
VictorKmin <kminvictor@gmail.com>
2019-08-13 19:22:55 +0300

    peactice

--- lesson1/scripts/script.js
+++ lesson1/scripts/script.js
@@ -1,83 +1,133 @@
-let name = "Viktor";
-let age = 22;
-let car = false;
+// let name = "Viktor";
+// let age = 22;
+// let car = false;
+//
+// //Стрічокив йкоментар
+//
+// /**
+//  * цей метод використову.ґжсятьсяс іа аіа
+//  */
+//
+// /* console.log(name);
+// Блочний коментар
+//  */
+//
+//
+// // переоголошення та document.write vs .log
+// let motorMyCar = 7;
+//
+// console.log(motorMyCar);
+//
+// motorMyCar = null;
+//
+// document.write(motorMyCar);
+//
+//
+// // CONST
+// const THIS_IS_CONST = 3.1415999999999999;
+//
+// const E = 2.71;
+//
+// // THIS_IS_CONST = 3;
+// // console.log(THIS_IS_CONST);
+//
+// let one = 0.1;
+// let two = 0.2;
+// console.log(one + two);
+//
+// // DONT USE THIS PLEEEASE! :((
+// var xxx = 'This is var';
+// console.log('________________');
+// console.log(xxx.length);
+// console.log('________________');
+//
+// // математика!
+// console.log(1 + 2);
+//
+// console.log(20 - 7);
+//
+// console.log(3 * 2);
+//
+// console.log(25 / 5);
+//
+// // console.log(10 % 3); // 1
+//
+// console.log(8 % 2);
+// console.log(23 % 3);
+//
+// console.log("2" + 2);
+// console.log('Okten' + 'Web');
+// console.log("2" - 2);
+// console.log("2" * 2);
+// console.log("2" / 2);
+// console.log("okten" + 2);
+// console.log("okten" * 2);
+// console.log(typeof NaN);
+// console.log(2 + '');
+//
+// console.log(
+//     (2 + 2) * 2
+// )
+//
+// // string -> num; num -> string
+// let stringFromNumber = (77).toString();
+// console.log(stringFromNumber);
+// let numberFromString = parseInt('777');
+// let numberFromString2 = +'777';
+//
+// let okten = +'okten';
+//
+// console.log(numberFromString);
+// console.log(numberFromString2);
+//
+// console.log(okten);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// //
+// let var1 = prompt('Enter number 1', 23456789);
+//
+// var1 = parseInt(var1);
+//
+// console.log(var1);
 
-//Стрічокив йкоментар
-
-/**
- * цей метод використову.ґжсятьсяс іа аіа
- */
-
-/* console.log(name);
-Блочний коментар
- */
+// let var2 = prompt('Enter number 2', 'ENTER ME');
+//
+// const RESULT = (var1 * var1) + 2 * var1 *var2 + (var2 * var2);
+//
+//
+// alert('RESULT IS ' + RESULT);
+// console.log(RESULT);
 
 
-// переоголошення та document.write vs .log
-let motorMyCar = 7;
+let x = 789; // 987
 
-console.log(motorMyCar);
+let n1 = x % 100;
+n1 = parseInt(n1 / 10);
 
-motorMyCar = null;
+let n2 = x % 10;
 
-document.write(motorMyCar);
+let n3 = parseInt(x / 100);
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+
+console.log(n2 + '' + n1 + '' + n3);
+console.log('It\'s perfect');
+console.log("___________________");
 
 
-// CONST
-const THIS_IS_CONST = 3.1415999999999999;
-
-const E = 2.71;
-
-// THIS_IS_CONST = 3;
-// console.log(THIS_IS_CONST);
-
-let one = 0.1;
-let two = 0.2;
-console.log(one + two);
-
-// DONT USE THIS PLEEEASE! :((
-var xxx = 'This is var';
-console.log('________________');
-console.log(xxx.length);
-console.log('________________');
-
-// математика!
-console.log(1 + 2);
-
-console.log(20 - 7);
-
-console.log(3 * 2);
-
-console.log(25 / 5);
-
-// console.log(10 % 3); // 1
-
-console.log(8 % 2);
-console.log(23 % 3);
-
-console.log("2" + 2);
-console.log('Okten' + 'Web');
-console.log("2" - 2);
-console.log("2" * 2);
-console.log("2" / 2);
-console.log("okten" + 2);
-console.log("okten" * 2);
-console.log(typeof NaN);
-console.log(2 + '');
-
-console.log(
-    (2 + 2) * 2
-)
-
-// string -> num; num -> string
-let stringFromNumber = (77).toString();
-console.log(stringFromNumber);
-let numberFromString = parseInt('777');
-let numberFromString2 = +'777';
-
-let okten = +'okten';
-
-console.log(numberFromString);
-console.log(numberFromString2);
-
-console.log(okten);
+let x = 10, y = 20;
